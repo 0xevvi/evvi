6321fbac4e46bb327e56eeeedaf49d66b290b6d1
0xevvi <0xevvi@proton.me>
2025-02-10 12:47:05 +0000

    Quartz sync: Feb 10, 2025, 12:47 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,7 +15,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="0xEvvi">0xEvvi</a> © {year}
+          <a href="https://0xevvi.github.io/evvi/About">0xEvvi</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
